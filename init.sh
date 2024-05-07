80059ac58e29ccb01f632476ca3bc42737dd56c6
Maksim <trukhinmaksimof@mail.ru>
2024-05-07 14:20:34 +0800

    Max 2024-05-07 14:20:34

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 git init
 git checkout -b Max
 git remote add origin git@github.com:maksimphono/Big_Data_Lab02.git
+git add -A
+git commit -m "init"
 git fetch origin
-git merge -X theirs Max
+git merge --allow-unrelated-histories -X theirs origin/Max
