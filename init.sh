7975d10e995c606ba01aac490d1eff67f4d6e81e
Maksim <trukhinmaksimof@mail.ru>
2024-05-07 14:18:34 +0800

    init

---
+++ init.sh
@@ -0,0 +1,8 @@
+git init
+git checkout -b Max
+git remote add origin git@github.com:maksimphono/Big_Data_Lab02.git
+git add -A
+git commit -m "init"
+git fetch origin
+git merge -X theirs origin/Max
+
